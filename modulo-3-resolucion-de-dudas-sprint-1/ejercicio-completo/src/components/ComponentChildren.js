67db0cdb074a38fd6ecfea3c89c3acac020ce02d
Iván (profe) <ivan@adalab.es>
2021-02-19 15:57:54 +0100

    Add exercise showing a card.

---
+++ modulo-3-resolucion-de-dudas-sprint-1/ejercicio-completo/src/components/ComponentChildren.js
@@ -0,0 +1,10 @@
+function ComponentChildren(props) {
+  return (
+    <section>
+      <h1>Titulo</h1>
+      <p>Descripción de la serie</p>
+    </section>
+  );
+}
+
+export default ComponentChildren;
